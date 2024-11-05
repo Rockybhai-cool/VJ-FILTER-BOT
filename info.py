e0f965c7f8d2c60fc96c577e9bab6da637c3b02e
Rockybhai-cool <coolcriminal786@gmail.com>
2024-11-05 18:36:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://coolcriminal786:8ccSlvLLNJVsPWpX@cluster0.idhak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "movie pirates ")
+DATABASE_NAME = environ.get('DATABASE_NAME', "moviepirates")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'realpirates')
 
 # Premium And Referal Settings
